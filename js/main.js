22acc179c427cb16069a9a0e164714f5c8f13c1f
Agent208 Site69 <agent208@site69.ru>
2022-02-16 00:12:00 +0300

    Create skelet of project

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+	// Mmenu init
+    // menu = new Mmenu("#js-main-menu", {
+	// 	"navbars": [
+	// 		{
+	// 			"position": "bottom",
+	// 			"content": [
+	// 				"<div class='u-d-flex u-flex-column'><div class='text-secondary'>Звонок по России бесплатный</div><a href='tel:88002004845' class='phone-main u-color-blue'>8 800-200-48-45</a></div>"
+	// 			]
+	// 		}
+	// 	],
+	// 	extensions: [
+	// 		"pagedim-black",
+	// 	],
+	// 	navbar: {
+	// 		title: "<img src='/img/logo-header.svg' alt='logo'>"
+	// 	}
+	// },
+	// {
+	// 	offCanvas: {
+	// 		clone: true
+	// 	}
+	// });
+});
